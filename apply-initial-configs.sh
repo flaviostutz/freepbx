95b08c0a1a177d72bcb92077956a75f813c6be4a
fsalomon <github@salo.mailrange.com>
2022-09-20 03:45:16 +0200

    Update apply-initial-configs.sh
    Don't know how it got into that state by my container couldn't start because /tmp/modulejson was non-empty, so I added a line to remove it.

--- apply-initial-configs.sh
+++ apply-initial-configs.sh
@@ -40,6 +40,7 @@ sed -i 's/"externip": "186.195.33.54"/"externip": "'$SIP_NAT_IP'"/' Sipsettings.
 echo "Assembling updated backup archive..."
 
 cd /tmp/basic-config
+rm -r /tmp/modulejson
 mv modulejson /tmp/
 mkdir modulejson
 mv /tmp/modulejson/Framework.json modulejson/
